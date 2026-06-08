09c28e2920b863fe5f31d67598523c1a76d48d74
averagecryptid <grtahsuda@gmail.com>
2026-06-07 19:14:20 -0500

    edit js

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -17,11 +17,11 @@ function w3_open() {
   document.getElementById("main").style.marginLeft = "25%";
   document.getElementById("sidebar").style.width = "25%";
   document.getElementById("sidebar").style.display = "block";
-  document.getElementById("openNav").style.display = 'none';
+  document.getElementById("nav").style.display = 'none';
 }
 
 function w3_close() {
   document.getElementById("main").style.marginLeft = "0%";
-  document.getElementById("mySidebar").style.display = "none";
-  document.getElementById("openNav").style.display = "inline-block";
+  document.getElementById("sidebar").style.display = "none";
+  document.getElementById("nav").style.display = "inline-block";
 } 
