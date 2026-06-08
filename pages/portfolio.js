39f939fcafe8ed3d35e57ecc0f2e454749134182
averagecryptid <grtahsuda@gmail.com>
2026-06-07 19:07:15 -0500

    edit html css

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -12,3 +12,16 @@ function filterImages(category) {
         }
     });
 }
+
+function w3_open() {
+  document.getElementById("main").style.marginLeft = "25%";
+  document.getElementById("sidebar").style.width = "25%";
+  document.getElementById("sidebar").style.display = "block";
+  document.getElementById("openNav").style.display = 'none';
+}
+
+function w3_close() {
+  document.getElementById("main").style.marginLeft = "0%";
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("openNav").style.display = "inline-block";
+} 
