1b84a7f19c5a08c03f5cc24f95256a0d03ae31cc
averagecryptid <grtahsuda@gmail.com>
2026-06-08 13:31:30 -0500

    edit

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -21,6 +21,6 @@ function w3_open() {
 
 function w3_close() {
     document.getElementById("sidebar").style.display = "none";
-    document.getElementById("myOverlay").style.display = "none";
-    document.getElementById("main-content").style.marginLeft = "250px";
+   document.getElementById("myOverlay").style.display = "none";
+    document.getElementById("main-content").style.marginLeft = "0px";
 }
