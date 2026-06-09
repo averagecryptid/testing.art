c6d7b3736740d1358c6a6f0ff15d90e2865f910c
averagecryptid <grtahsuda@gmail.com>
2026-06-08 22:16:25 -0500

    html js

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -42,3 +42,10 @@ function w3_close() {
     document.getElementById("myOverlay").style.display = "none";
     document.getElementById("main-content").style.marginLeft = "0px";
 }
+
+function onClick(element) {
+    document.getElementById("img01").src = element.src;
+    document.getElementById("modal01").style.display = "block";
+    var captionText = document.getElementById("caption");
+    captionText.innerHTML = element.alt;
+}
